bef6bc36fd4b3f419dd4e3e1f2961bb6cb311ad2
nuardankr <nuardankr@gmail.com>
2022-07-19 10:58:35 +0900

    Initial commit

---
+++ 999. Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_test
+{
+    class Program
+    {   
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Age : ");
+            int age = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("you are {0} years old",age);
+        }
+    }
+}
